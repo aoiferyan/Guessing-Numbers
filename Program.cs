af2a1616edef2145b3658371e37286894a74fc74
Aoife Ryan <aoife.margaret.ryan@gmail.com>
2020-01-21 14:06:27 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Guessing__Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //set the variables
+            String Name = "Guessing Numbers Game";
+            String Version = "1.0";
+            String Author = "Aoife Ryan";
+
+            //change the text colour for the header
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            //Write to the console
+            Console.WriteLine("{0}: Version {1} By {2}", Name, Version, Author);
+
+            //Resets coour to default
+            Console.ResetColor();
+        }
+    }   
+}
