d682512ec4f9c3be525b3705fca72212580a1e6d
Aoife Ryan <aoife.margaret.ryan@gmail.com>
2020-01-27 22:48:53 +0000

    Added while loop and if statement

--- Program.cs
+++ Program.cs
@@ -19,6 +19,49 @@ namespace Guessing__Numbers
 
             //Resets coour to default
             Console.ResetColor();
+
+            //Ask users name
+            Console.WriteLine("What is your name?");
+
+            //Get user input name
+            string username = Console.ReadLine();
+
+            //welcome the user by name
+            Console.WriteLine($"Hello {username}! Let's play a game......");
+
+
+            //declare property
+            int correctnumber = 7;
+            int number = 0;
+
+            //ask user for number
+            Console.WriteLine("Guess a number between 1 and 10....");
+
+            //if number is wrong
+            while (number != correctnumber)
+            {
+                string input = Console.ReadLine();
+
+                number = Convert.ToInt32(input);
+
+                //match number to correctnumber
+                if (number != correctnumber)
+                {
+                    //change the text colour for the header
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    //Write to the console
+                    Console.WriteLine("Wrong number! please try again");
+
+                    //Resets coour to default
+                    Console.ResetColor();
+                }
+
+            }
+
+            //If correct
+            Console.WriteLine("You are correct");
+
         }
     }   
 }
