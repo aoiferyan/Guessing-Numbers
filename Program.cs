372e5456516a7714795b8b1ed5eb2c08865382df
Aoife Ryan <aoife.margaret.ryan@gmail.com>
2020-01-30 20:47:10 +0000

    Finished Simple Game. Added while loop and tidied code into methods.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,87 @@ namespace Guessing__Numbers
     {
         static void Main(string[] args)
         {
+            //Method called for app info
+            GetAppInfo();
+
+            //method called to greet user
+            GreetUser();
+
+            //while loop
+            while (true)
+            {
+                //declare property
+                //int correctnumber = 7;
+
+                //create a random number
+                Random random = new Random();
+
+                int correctnumber = random.Next(1, 10);
+
+
+                int number = 0;
+
+                //ask user for number
+                Console.WriteLine("Guess a number between 1 and 10....");
+
+                //if number is wrong
+                while (number != correctnumber)
+                {
+                    string input = Console.ReadLine();
+
+                    if (!int.TryParse(input, out number))
+                    {
+                        
+                        //Print error message
+                        PrintColourMessage(ConsoleColor.Red, "This isn't a number! Try again...");
+
+                        //keep going
+                        continue;
+                    }
+
+                    number = Convert.ToInt32(input);
+
+                    //match number to correctnumber
+                    if (number != correctnumber)
+                    {
+
+                        //Print error message
+                        PrintColourMessage(ConsoleColor.Red, "Wrong number!please try again");
+                      
+                    }
+
+                }
+
+                //Print error message
+                PrintColourMessage(ConsoleColor.Green, "You are Correct!!!!!");
+
+                Console.ReadLine();
+
+                //ask to play again?
+                Console.WriteLine("Do you want to play again? [Y Or N]");
+
+                //Get Answer
+                string answer = Console.ReadLine().ToUpper();
+
+
+                if(answer == "Y")
+                {
+                    continue;
+                }
+                else if(answer == "N")
+                {
+                    return;
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+        //Method with App Information
+        static void GetAppInfo()
+        {
             //set the variables
             String Name = "Guessing Numbers Game";
             String Version = "1.0";
@@ -20,6 +101,11 @@ namespace Guessing__Numbers
             //Resets coour to default
             Console.ResetColor();
 
+        }
+
+        //Method that greets user
+        static void GreetUser()
+        {
             //Ask users name
             Console.WriteLine("What is your name?");
 
@@ -29,69 +115,20 @@ namespace Guessing__Numbers
             //welcome the user by name
             Console.WriteLine($"Hello {username}! Let's play a game......");
 
+        }
 
-            //declare property
-            //int correctnumber = 7;
+        //Method to print colour message
 
-            //create a random number
-            Random random = new Random();
-
-            int correctnumber = random.Next(1, 10);
-
-
-            int number = 0;
-
-            //ask user for number
-            Console.WriteLine("Guess a number between 1 and 10....");
-
-            //if number is wrong
-            while (number != correctnumber)
-            {
-                string input = Console.ReadLine();
-
-                if(!int.TryParse(input, out number))
-                {
-                    //change the text colour for the header
-                    Console.ForegroundColor = ConsoleColor.Red;
-
-                    //Write to the console
-                    Console.WriteLine("This isn't a number! Try again...");
-
-                    //Resets coour to default
-                    Console.ResetColor();
-
-                    //keep going
-                    continue;
-                }
-
-                number = Convert.ToInt32(input);
-
-                //match number to correctnumber
-                if (number != correctnumber)
-                {
-                    //change the text colour for the header
-                    Console.ForegroundColor = ConsoleColor.Red;
-
-                    //Write to the console
-                    Console.WriteLine("Wrong number! please try again");
-
-                    //Resets coour to default
-                    Console.ResetColor();
-                }
-
-            }
-      
+        static void PrintColourMessage(ConsoleColor colour, string message)
+        {
             //change the text colour for the header
-            Console.ForegroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = colour;
 
             //Write to the console
-            Console.WriteLine("You are Correct!!!!!");
+            Console.WriteLine(message);
 
             //Resets coour to default
             Console.ResetColor();
-
-            Console.ReadLine();
-
         }
     }   
 }
